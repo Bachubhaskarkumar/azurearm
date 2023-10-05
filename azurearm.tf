89f347b243138f1216d62d5e9c62753535046129
Bachubhaskarkumar <80734795+Bachubhaskarkumar@users.noreply.github.com>
2023-10-05 14:51:48 +0530

    Update azurearm.tf

--- azurearm.tf
+++ azurearm.tf
@@ -1,188 +1,49 @@
-terraform {
-  required_version = ">=1.0"
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~>3.0"
-    }
+provider "azurerm" {
+  features {}
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "example" {
+  name     = "myResourceGroup"
+  location = "East US"
+}
+
+# Create an Azure Key Vault to store secrets
+resource "azurerm_key_vault" "example" {
+  name                       = "mykeyvault"
+  location                   = azurerm_resource_group.example.location
+  resource_group_name        = azurerm_resource_group.example.name
+  sku_name                   = "standard"
+  tenant_id                  = data.azurerm_client_config.current.tenant_id
+  enabled_for_disk_encryption = true
+  enabled_for_template_deployment = true
+  enabled_for_deployment       = true
+  enabled_for_visual_studio    = true
+}
+
+# Store Azure Service Principal credentials in Key Vault
+resource "azurerm_key_vault_secret" "sp_credentials" {
+  name         = "sp-credentials"
+  value        = "azurecredentials"  # Store this secret in Jenkins or use another secure method
+  key_vault_id = azurerm_key_vault.example.id
+}
+
+# Define your virtual machine configuration using ARM templates
+resource "azurerm_template_deployment" "example" {
+  name                = "example-deployment"
+  resource_group_name = azurerm_resource_group.example.name
+  template_content    = file("arm-template.json")  # Path to your ARM template file
+  parameter {
+    name  = "adminUsername"
+    value = "adminuser"
+  }
+  parameter {
+    name  = "adminPassword"
+    value = "P@ssw0rd123!"  # Store this secret in Jenkins or use another secure method
   }
 }
-resource "azurerm_resource_group" "bhaskar-rg" {
-  name     = "bhaskar-rg"
-  location = "EAST US"
-}
-resource "azurerm_resource_group_template_deployment" "deploy" {
-  name                = "deploy"
-  resource_group_name = azurerm_resource_group.bhaskar-rg.name
-  template_body = file("${path.module}/deploymentTemplate.json")
 
-   template_content = <<TEMPLATE
-{
-    "$schema": "https://schema.management.azure.com/schemas/2019-04-01/deploymentTemplate.json#",
-    "contentVersion": "1.0.0.0",
-    "parameters": {},
-    "functions": [],
-    "variables": {},
-    "resources": [
-        {
-            "name": "vnet",
-            "type": "Microsoft.Network/virtualNetworks",
-            "apiVersion": "2020-11-01",
-            "location": "EAST US",
-            "properties": {
-                "addressSpace": {
-                    "addressPrefixes": [
-                        "10.0.0.0/16"
-                    ]
-                },
-                "subnets": [
-                    {
-                        "name": "SubnetA",
-                        "properties": {
-                            "addressPrefix": "10.0.0.0/24",
-                            "networkSecurityGroup": {
-                                "id": "[resourceId('Microsoft.Network/networkSecurityGroups', 'nsg')]"
-                              }
-                        }
-                    }
-                ]
-            }
-        },
-        {
-            "name": "public-ip",
-            "type": "Microsoft.Network/publicIPAddresses",
-            "apiVersion": "2020-11-01",
-            "location": "EAST US",
-            "properties": {
-                "publicIPAllocationMethod": "Dynamic"
-                }
-            },
-            {
-                "name": "nic",
-                "type": "Microsoft.Network/networkInterfaces",
-                "apiVersion": "2020-11-01",
-                "location": "EAST US",            
-                "properties": {
-                    "ipConfigurations": [
-                        {
-                            "name": "ipConfig",
-                            "properties": {
-                                "privateIPAllocationMethod": "Dynamic",
-                                "publicIPAddress": {
-                                    "id": "[resourceId('Microsoft.Network/publicIPAddresses', 'public-ip')]"
-                                  },
-                                "subnet": {
-                                    "id": "[resourceId('Microsoft.Network/virtualNetworks/subnets', 'vnet', 'SubnetA')]"
-                                }
-                            }
-                        }
-                    ]
-                },
-                "dependsOn": [
-                    "[resourceId('Microsoft.Network/publicIPAddresses', 'public-ip')]",
-                    "[resourceId('Microsoft.Network/virtualNetworks', 'vnet')]"
-                ]
-            }
-        },        
-        {
-            "name": "harsha1512",
-            "type": "Microsoft.Storage/storageAccounts",
-            "apiVersion": "2021-04-01",
-            "location": "[resourceGroup().location]",
-            "sku": {
-                "name": "Standard_LRS"
-            },
-            "kind": "StorageV2"
-        },
-        {
-            "name": "nsg",
-            "type": "Microsoft.Network/networkSecurityGroups",
-            "apiVersion": "2020-11-01",
-            "location": "[resourceGroup().location]",
-            "properties": {
-                "securityRules": [
-                    {
-                        "name": "Allow-RDP",
-                        "properties": {
-                            "description": "description",
-                            "protocol": "Tcp",
-                            "sourcePortRange": "*",
-                            "destinationPortRange": "3389",
-                            "sourceAddressPrefix": "*",
-                            "destinationAddressPrefix": "*",
-                            "access": "Allow",
-                            "priority": 100,
-                            "direction": "Inbound"
-                        }
-                    }
-                ]
-            }
-        },
-        {
-            "name": "vm",
-            "type": "Microsoft.Compute/virtualMachines",
-            "apiVersion": "2021-03-01",
-            "location": "[resourceGroup().location]",
-            "dependsOn": [
-                "[resourceId('Microsoft.Storage/storageAccounts', toLower('harsha1512'))]"
-            ],
-            "properties": {
-                "hardwareProfile": {
-                    "vmSize": "Standard_D2s_v3"
-                },
-                "osProfile": {
-                    "computerName": "vm",
-                    "adminUsername": "demousr",
-                    "adminPassword": "[parameters('secret')]"
-                },
-                "storageProfile": {
-                    "imageReference": {
-                        "publisher": "MicrosoftWindowsServer",
-                        "offer": "WindowsServer",
-                        "sku": "2019-Datacenter",
-                        "version": "latest"
-                    },
-                    "osDisk": {
-                        "name": "windowsVM1OSDisk",
-                        "caching": "ReadWrite",
-                        "createOption": "FromImage"
-                    },
-                    "dataDisks": [
-                        {
-                            "name":"vm-data-disk",
-                            "diskSizeGB":16,
-                            "createOption": "Empty",
-                            "lun":0
-                        }
-                    ]
-                },
-                "networkProfile": {
-                    "networkInterfaces": [
-                        {
-                            "id": "[resourceId('Microsoft.Network/networkInterfaces', 'nic')]"
-                        }
-                    ]
-                },
-                "diagnosticsProfile": {
-                    "bootDiagnostics": {
-                        "enabled": true,
-                        "storageUri": "[reference(resourceId('Microsoft.Storage/storageAccounts/', toLower('harsha1512'))).primaryEndpoints.blob]"
-                    }
-                }
-            }
-        }
-    },
-    "outputs": {},
-}
-TEMPLATE
-
-  # these key-value pairs are passed into the ARM Template's `parameters` block
-  
-  parameters_content = jsondecode({
-    "secret" = {
-        value = azurerm_key_vault_secret.secret.value
-        }
-  })
-
-  deployment_mode = "Incremental"
+# Output the public IP address of the VM
+output "public_ip" {
+  value = azurerm_template_deployment.example.outputs["adminUsername"]
 }
