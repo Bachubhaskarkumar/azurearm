8075f36d29a1dcf4bd53e21639291c51333f7f5a
Bachubhaskarkumar <80734795+Bachubhaskarkumar@users.noreply.github.com>
2023-10-05 13:05:12 +0530

    Update azurearm.tf

--- azurearm.tf
+++ azurearm.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_version = ">=1.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+}
 resource "azurerm_resource_group" "bhaskar-rg" {
   name     = "bhaskar-rg"
   location = "EAST US"
