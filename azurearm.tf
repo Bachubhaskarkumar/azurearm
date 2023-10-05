baaea3032a12c0a9df4fc191d1593d6436af2ea2
Bachubhaskarkumar <80734795+Bachubhaskarkumar@users.noreply.github.com>
2023-10-05 17:28:48 +0530

    Update azurearm.tf

--- azurearm.tf
+++ azurearm.tf
@@ -47,11 +47,11 @@ resource "azurerm_resource_group_template_deployment" "example" {
   resource_group_name = azurerm_resource_group.example.name
   #template_content    = file("arm-template.json")  # Path to your ARM template file
   deployment_mode     = "Incremental"
-  parameters_content = {
+  parameters_content = jsonencode({
     adminUsername = "adminuser"
     adminPassword = "P@ssw0rd123!"  # Store this secret in Jenkins or use another secure method
     subnetId = "/subscriptions/****/resourceGroups/mybhaskar/providers/Microsoft.Network/virtualNetworks/myNIC/subnets/mySubnet"
-  }
+  })
   #template_body       = <<DEPLOY
   template_content     = <<TEMPLATE
 {
