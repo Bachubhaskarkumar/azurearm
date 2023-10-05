caf13f573e6542daa1595659cfd9690ee6eeae0a
Bachubhaskarkumar <80734795+Bachubhaskarkumar@users.noreply.github.com>
2023-10-05 16:17:59 +0530

    Update azurearm.tf

--- azurearm.tf
+++ azurearm.tf
@@ -19,6 +19,16 @@ resource "azurerm_key_vault" "example" {
   enabled_for_disk_encryption = true
   enabled_for_template_deployment = true
   enabled_for_deployment       = true
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+    secret_permissions = [
+      "Get",
+      "Set",
+      "Delete",
+      "Recover"
+    ]
+  }
 }
 
 # Store Azure Service Principal credentials in Key Vault
